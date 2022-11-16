c15b5a86efcb46d9961e3d24bc48b6aca4277aee
Alfredo-GonzalezA <alfredo.gonzalez.338@my.csun.edu>
2022-11-16 12:24:42 -0800

    11/16/21 First commit

---
+++ app/src/main/java/com/example/rexpet_careapp/PetSitting.java
@@ -0,0 +1,14 @@
+package com.example.rexpet_careapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PetSitting extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pet_sitting);
+    }
+}
