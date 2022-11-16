20d65434424bf940e0e3c948a329180cbfa3c7c7
Alfredo-GonzalezA <alfredo.gonzalez.338@my.csun.edu>
2022-11-16 15:14:42 -0800

    Added functions to login and signup page. Needs login authentication and signup info needs to be saved

--- app/src/main/java/com/example/rexpet_careapp/Login.java
+++ app/src/main/java/com/example/rexpet_careapp/Login.java
@@ -2,13 +2,43 @@ package com.example.rexpet_careapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
 
+    Button switchToLogin;
+    Button switchToMain;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        switchToLogin = findViewById(R.id.signupbutton);
+        switchToLogin.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                switchActivity();
+            }
+        });
+
+        switchToMain = findViewById(R.id.loginbutton);
+        switchToMain.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                switchToMainActivity();
+            }
+        });
+    }
+    private void switchActivity(){
+        Intent switchActivityIntent = new Intent(this, SignUp.class);
+        startActivity(switchActivityIntent);
+    }
+
+    private void switchToMainActivity(){
+        Intent switchMainIntent = new Intent(this,MainActivity.class);
+        startActivity(switchMainIntent);
     }
 }
--- app/src/main/java/com/example/rexpet_careapp/SignUp.java
+++ app/src/main/java/com/example/rexpet_careapp/SignUp.java
@@ -2,13 +2,20 @@ package com.example.rexpet_careapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class SignUp extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+    }
+
+    public void switchToLogin(View view){
+        startActivity(new Intent(this,Login.class));
     }
 }
