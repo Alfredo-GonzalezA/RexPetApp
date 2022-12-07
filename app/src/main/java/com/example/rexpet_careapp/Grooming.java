daa4998bd51d2cb1952cbc12dc6aaf9fd9e33c7d
Alfredo-GonzalezA <alfredo.gonzalez.338@my.csun.edu>
2022-12-06 21:04:37 -0800

    Finishing touches to the app

--- app/src/main/java/com/example/rexpet_careapp/Grooming.java
+++ app/src/main/java/com/example/rexpet_careapp/Grooming.java
@@ -2,22 +2,27 @@ package com.example.rexpet_careapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.TimePicker;
 
 import java.util.ArrayList;
+import java.util.Locale;
 
 public class Grooming extends AppCompatActivity {
-
+    Button timeButton;
+    int hour, minute;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_grooming);
-
 
         //this block of code is to add the spinner options for the types of grooming
         ArrayList<String> groomingarraylist = new ArrayList<>();
@@ -42,7 +47,7 @@ public class Grooming extends AppCompatActivity {
             }
         });
         //end block of code for spinner
-
+        timeButton = findViewById(R.id.time_picker_button);
     }
 
     public void switchToMainActivity(View view){
@@ -51,5 +56,31 @@ public class Grooming extends AppCompatActivity {
 
     public void continues(View view){
         startActivity(new Intent(this,FindAndSelectServiceProvider.class));
+    }
+
+    public void popupTimeClicker(View view) {
+        TimePickerDialog.OnTimeSetListener onTimeSetListener = new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
+                hour = selectedHour;
+                minute = selectedMinute;
+                if(hour > 12){
+                    timeButton.setText(String.format(Locale.getDefault(), "%02d:%02d PM", hour%12, minute));
+                }else if(hour == 0){
+                    timeButton.setText(String.format(Locale.getDefault(), "%02d:%02d AM", 12, minute));
+                }else if(hour == 12){
+                    timeButton.setText(String.format(Locale.getDefault(), "%02d:%02d PM", 12, minute));
+                }else {
+                    timeButton.setText(String.format(Locale.getDefault(), "%02d:%02d AM", hour, minute));
+                }
+
+            }
+        };
+
+        int style = AlertDialog.THEME_HOLO_DARK;
+
+        TimePickerDialog timePickerDialog = new TimePickerDialog(this, style, onTimeSetListener, hour, minute, false);
+        timePickerDialog.setTitle("Select Time");
+        timePickerDialog.show();
     }
 }
